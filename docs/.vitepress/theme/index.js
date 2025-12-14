a88b5eddf3861450054a310266cf5cc89c889976
Yuto Tagashira <ut0s.dev@gmail.com>
2025-12-14 12:05:33 +0900

    feat: change prime color from default to yellow

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import DefaultTheme from "vitepress/theme";
 import vitepressBackToTop from "vitepress-plugin-back-to-top";
 import "vitepress-plugin-back-to-top/dist/style.css";
 import "./custom.css"; // for tailwind css
+import "./colors.css"; // for custom colors
 
 export default {
   ...DefaultTheme,
